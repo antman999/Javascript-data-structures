b3eccc4ce1e73c4cd6ecffa1f5162d95cf5658de
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-11 11:55:10 -0400

    Added Push method

--- LinkedList.js
+++ LinkedList.js
@@ -1,6 +1,27 @@
-class Node{
-  constructor(value) {
-    this.value = value 
-    this.next = null 
-  }
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class SinglyLinkedList {
+	constructor() {
+		this.length = 0;
+		this.head = null;
+		this.tail = null;
+	}
+
+	push(value) {
+		let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode 
+      this.tail = newNode
+    }
+    this.length++ 
+    return this 
+	}
 }
