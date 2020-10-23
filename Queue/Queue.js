1d3b0433175905a64bcd2d395a4c7b66a767f2f6
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-23 14:51:11 -0400

    Added class structure

---
+++ Queue/Queue.js
@@ -0,0 +1,15 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+        this.size = 0
+    }
+}
+
+class Queue {
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
+  }
+}
