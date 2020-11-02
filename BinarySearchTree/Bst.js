2067f022cdd7a964032ec58b6f345443fba4f45c
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-11-02 15:29:09 -0500

    Implemented BFS

--- BinarySearchTree/Bst.js
+++ BinarySearchTree/Bst.js
@@ -17,27 +17,38 @@ class BinarySearchTree {
 		if (!this.root) {
 			this.root = newValue;
 			return this;
-    } else {
-      var  current = this.root 
-      while (true) {
-        if (newValue < current.value) {
-          if (current.left === null) {
-            current.left = newValue
-            return this
-          } else {
-            current = current.left 
-          }
-        } else if (newValue > current.value) {
-          if (current.right === null) {
-            current.right = newValue
-            return this 
-          } else {
-            current = current.right
-          }
-        }
-      }
-    }
-    
-   
+		} else {
+			var current = this.root;
+			while (true) {
+				if (newValue < current.value) {
+					if (current.left === null) {
+						current.left = newValue;
+						return this;
+					} else {
+						current = current.left;
+					}
+				} else if (newValue > current.value) {
+					if (current.right === null) {
+						current.right = newValue;
+						return this;
+					} else {
+						current = current.right;
+					}
+				}
+			}
+		}
 	}
+  bfs(){
+    let queue = []
+    let visited = []
+    let node;
+    queue.push(this.root)
+    while (queue.length) {
+      node = queue.shift()
+      visited.push(node)
+      if(node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right);
+    }
+    return visited
+  }
 }
