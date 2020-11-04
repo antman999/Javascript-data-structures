feced884977937002c985f4039c2863fbc992b0a
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-11-04 15:12:29 -0500

    Added solution for DFS post order

--- BinarySearchTree/Bst.js
+++ BinarySearchTree/Bst.js
@@ -1,8 +1,8 @@
 class Node {
 	constructor(value) {
-	 this.value = value;
-	 this.left = left;
-	 this.right = right;
+		this.value = value;
+		this.left = left;
+		this.right = right;
 	}
 }
 
@@ -11,34 +11,34 @@ class BinarySearchTree {
 		this.root = null;
 	}
 
- insert(value) {
-	let newValue = new Node(value);
+	insert(value) {
+		let newValue = new Node(value);
 
-	if (!this.root) {
-		this.root = newValue;
-		return this;
-	} else {
-	  var current = this.root;
-		while (true) {
-		 if (newValue < current.value) {
-			if (current.left === null) {
-			 current.left = newValue;
-			 return this;
-			} else {
-			 current = current.left;
-		  }
-		} else if (newValue > current.value) {
-		   if (current.right === null) {
-				current.right = newValue;
-			  return this;
-			} else {
-			current = current.right;
-			 }
+		if (!this.root) {
+			this.root = newValue;
+			return this;
+		} else {
+			var current = this.root;
+			while (true) {
+				if (newValue < current.value) {
+					if (current.left === null) {
+						current.left = newValue;
+						return this;
+					} else {
+						current = current.left;
+					}
+				} else if (newValue > current.value) {
+					if (current.right === null) {
+						current.right = newValue;
+						return this;
+					} else {
+						current = current.right;
+					}
+				}
 			}
-	  }
+		}
 	}
- }
-	
+
 	BFS() {
 		let queue = [];
 		let visited = [];
@@ -51,17 +51,29 @@ class BinarySearchTree {
 			if (node.right) queue.push(node.right);
 		}
 		return visited;
-  }
-  
-  DFSPreOrder() {
-    let visited = []
-    let current = this.root
-    const helperFunction = node => {
-      visited.push(node)
-      if(node.left) helperFunction(node.left)
-      if (node.right) helperFunction(node.right);
-    }
-    helperFunction(current)
-    return visited
-  }
+	}
+
+	DFSPreOrder() {
+		let visited = [];
+		let current = this.root;
+		const helperFunction = node => {
+			visited.push(node);
+			if (node.left) helperFunction(node.left);
+			if (node.right) helperFunction(node.right);
+		};
+		helperFunction(current);
+		return visited;
+	}
+
+	DFSPostOrder() {
+		let visited = []
+		let current = this.root; 
+		const helper = node => {
+			if(node.left) helper(node.left)
+			if (node.right) helper(node.right)
+			visited.push(node.value)
+		}
+		helper(current)
+		return visited
+	}
 }
