af0e8b84e5d892e1f10c10c2ff6e6aaa8c39b7de
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-11-28 00:18:26 -0500

    Added min heap enqueue

--- heaps/PriorityQueue/PriorityQueue.js
+++ heaps/PriorityQueue/PriorityQueue.js
@@ -11,5 +11,19 @@ class PriorityQueue{
     this.values = []
   }
 
+  enqueue(val, priority) {
+    let node = new Node(val, priority)
+    this.values.push(node)
+    let index = this.values.length - 1;
+    let element = this.values[index]
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2)
+      let parent = this.values[parentIndex]
+      if (element.priority >= parent.priority) break;
+      this.values[parentIndex] = element
+      this.values[index] = parent
+      index = parentIndex
+    }
+}
 
 }
